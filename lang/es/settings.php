089009fea275710e485dfa79881dd8feaf0a7283
Rafa Couto <rafa@aplicacionesyredes.com>
2013-12-01 21:45:29 +0100

    Translation of plugin settings into Spanish and Galician.

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,26 +1,26 @@
 <?php
 
 // languages
-$lang['enabled_languages'] = "Enabled languages. Comma separated list of codes. Leave empty for auto.";
-$lang['guess_lang_by_ns'] = "If the namespace of a page is a valid language code, use it to guess the page language when unknown";
-$lang['guess_lang_by_ui_lang'] = "Use the wiki's user interface to auto-detect guess page language when unknown";
-$lang['default_original_language'] = "If the page language can not be detected, assume this language for pages";
-$lang['use_language_namespace'] = "Use language code as namespace for translations";
+$lang['enabled_languages'] = "Idiomas habilitados. Lista de códigos separados por coma. Dejar en blanco para automático.";
+$lang['guess_lang_by_ns'] = "Si el espacio de nombres de la página es un código de idioma válido, úsalo para inferir el idioma de la página cuando se desconozca.";
+$lang['guess_lang_by_ui_lang'] = "Usa la interfaz de usuario del wiki para inferir el idioma cuando no se conozca.";
+$lang['default_original_language'] = "Si el idioma de la página no se puede detectar, asume este idioma para las páginas";
+$lang['use_language_namespace'] = "Usa el código de idioma como espacio de nombres para las traducciones";
 
 // namespaces and pages
-$lang['include_namespaces'] = 'Comma separated list of namespaces where pages can be translated. Use * for the entire wiki.';
-$lang['exclude_namespaces'] = 'Comma separated list of namespaces to exclude';
-$lang['exclude_pagenames']  = 'Comma separated list of pagenames to exclude in any namespace';
+$lang['include_namespaces'] = 'Lista separada por comas de los espacios de nombres donde las páginas pueden ser traducidas. Usa * para todo el wiki.';
+$lang['exclude_namespaces'] = 'Lista separada por comas de los espacios de nombres a excluír';
+$lang['exclude_pagenames']  = 'Lista separada por comas de los nombres de página a excluír en cualquier espacio de nombres';
 
 // permissions
-$lang['translator_group'] = 'Give users in this group the right to create and edit translations, even if they only have read permission in ACL.';
-$lang['author_group'] = 'Give users in this group the right to create and edit original pages, i.e. not translated ones, even if they only have read permission in ACL.';
+$lang['translator_group'] = 'Dar a los usuarios en este grupo el permiso para crear y editar traducciones, aúnque sólo tengan permiso de lectura en las ACL.';
+$lang['author_group'] = 'Dar a los usuarios en este grupo el permiso para crear y editar páginas originales, por ejemplo las no traducidas, aúnque sólo tengan permiso de lectura en las ACL.';
 
 // appearence
-$lang['insert_translation_links'] = 'Insert translation links on page. Turn this off if you want to insert them in your template manually.';
-$lang['link_style'] = "Translation link style";
-$lang['link_style_o_langname'] = "Language name";
-$lang['link_style_o_langcode'] = "Language code";
+$lang['insert_translation_links'] = 'Incluir enlaces de traducción en la página. Desactiva esto si quieres incluirlas en la plantilla manualmente.';
+$lang['link_style'] = "Estilo del enlace de traducción";
+$lang['link_style_o_langname'] = "Nombre del idioma";
+$lang['link_style_o_langcode'] = "Código del idioma";
 
 // ....not used....
 //$lang['runas'] = 'Use this (virtual) user\'s permissions when checking ACLs for creating translation pages.';
--- lang/gl/settings.php
+++ lang/gl/settings.php
@@ -1,26 +1,26 @@
 <?php
 
 // languages
-$lang['enabled_languages'] = "Enabled languages. Comma separated list of codes. Leave empty for auto.";
-$lang['guess_lang_by_ns'] = "If the namespace of a page is a valid language code, use it to guess the page language when unknown";
-$lang['guess_lang_by_ui_lang'] = "Use the wiki's user interface to auto-detect guess page language when unknown";
-$lang['default_original_language'] = "If the page language can not be detected, assume this language for pages";
-$lang['use_language_namespace'] = "Use language code as namespace for translations";
+$lang['enabled_languages'] = "Linguas habilitadas. Lista de códigos separados por coma. Deixar baleiro para automático.";
+$lang['guess_lang_by_ns'] = "Se o espacio de nomes da páxina é un código de lingua válido, úsao para inferir a língua da páxina cando non se coñeza.";
+$lang['guess_lang_by_ui_lang'] = "Usa a interface de usuario do wiki para inferir a lingua cando non se coñeza.";
+$lang['default_original_language'] = "Se a lingua da páxina no se pode detectar, asume esta linuga para as páxinas";
+$lang['use_language_namespace'] = "Usa o código de lingua como espacio de nomes para as traducións";
 
 // namespaces and pages
-$lang['include_namespaces'] = 'Comma separated list of namespaces where pages can be translated. Use * for the entire wiki.';
-$lang['exclude_namespaces'] = 'Comma separated list of namespaces to exclude';
-$lang['exclude_pagenames']  = 'Comma separated list of pagenames to exclude in any namespace';
+$lang['include_namespaces'] = 'Lista separada por comas dos espacios de nomes onde as páxinas poden ser traducidas. Usa * para todo o wiki.';
+$lang['exclude_namespaces'] = 'Lista separada por comas dos espacios de nomes a excluír';
+$lang['exclude_pagenames']  = 'Lista separada por comas dos nomes de páxina a excluír en calquera espacio de nomes';
 
 // permissions
-$lang['translator_group'] = 'Give users in this group the right to create and edit translations, even if they only have read permission in ACL.';
-$lang['author_group'] = 'Give users in this group the right to create and edit original pages, i.e. not translated ones, even if they only have read permission in ACL.';
+$lang['translator_group'] = 'Dar aos usuarios neste grupo o permiso para crear e editar traducións, ainda que só teñan permiso de lectura nas ACL.';
+$lang['author_group'] = 'Dar aos usuarios neste grupo o permiso para crear e editar páxinas orixinais, por exemplo as non traducidas, ainda que só teñan permiso de lectura nas ACL.';
 
 // appearence
-$lang['insert_translation_links'] = 'Insert translation links on page. Turn this off if you want to insert them in your template manually.';
-$lang['link_style'] = "Translation link style";
-$lang['link_style_o_langname'] = "Language name";
-$lang['link_style_o_langcode'] = "Language code";
+$lang['insert_translation_links'] = 'Incluír ligazóns de tradución na páxina. Desactiva isto se queres incluílas no modelo manualmente.';
+$lang['link_style'] = "Estilo da ligazón de tradución";
+$lang['link_style_o_langname'] = "Nome da lingua";
+$lang['link_style_o_langcode'] = "Código da lingua";
 
 // ....not used....
 //$lang['runas'] = 'Use this (virtual) user\'s permissions when checking ACLs for creating translation pages.';
