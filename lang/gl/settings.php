58b857527ca2e2459b4406612a6591e946002c8e
Rafa Couto <rafa@aplicacionesyredes.com>
2013-12-01 21:09:14 +0100

    Galician translation support.

---
+++ lang/gl/settings.php
@@ -0,0 +1,29 @@
+<?php
+
+// languages
+$lang['enabled_languages'] = "Enabled languages. Comma separated list of codes. Leave empty for auto.";
+$lang['guess_lang_by_ns'] = "If the namespace of a page is a valid language code, use it to guess the page language when unknown";
+$lang['guess_lang_by_ui_lang'] = "Use the wiki's user interface to auto-detect guess page language when unknown";
+$lang['default_original_language'] = "If the page language can not be detected, assume this language for pages";
+$lang['use_language_namespace'] = "Use language code as namespace for translations";
+
+// namespaces and pages
+$lang['include_namespaces'] = 'Comma separated list of namespaces where pages can be translated. Use * for the entire wiki.';
+$lang['exclude_namespaces'] = 'Comma separated list of namespaces to exclude';
+$lang['exclude_pagenames']  = 'Comma separated list of pagenames to exclude in any namespace';
+
+// permissions
+$lang['translator_group'] = 'Give users in this group the right to create and edit translations, even if they only have read permission in ACL.';
+$lang['author_group'] = 'Give users in this group the right to create and edit original pages, i.e. not translated ones, even if they only have read permission in ACL.';
+
+// appearence
+$lang['insert_translation_links'] = 'Insert translation links on page. Turn this off if you want to insert them in your template manually.';
+$lang['link_style'] = "Translation link style";
+$lang['link_style_o_langname'] = "Language name";
+$lang['link_style_o_langcode'] = "Language code";
+
+// ....not used....
+//$lang['runas'] = 'Use this (virtual) user\'s permissions when checking ACLs for creating translation pages.';
+//$lang['notrans'] = 'Never translate pages matching this regular expression';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
